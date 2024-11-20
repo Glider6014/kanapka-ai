dddf9a6dca7fb005703a9f64c26a72102fb8ade1
Patryk-Ruzicki <arcysanta@gmail.com>
2024-11-20 14:21:36 +0100

    deleted SearchRecipes.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
-import { SearchRecipes } from "@/components/SearchRecipes";
+import { Navbar } from "@/components/Navbar";
+import { SearchPanel } from "@/components/SearchPanel";
+import { RecipesTable } from "@/components/RecipesTable";
 
 export default function Home() {
   return (
     <>
-      <SearchRecipes />
+      <div className="container mx-auto p-4">
+        <Navbar />
+        <div className="flex flex-col md:flex-row gap-4">
+          <SearchPanel />
+          <RecipesTable />
+        </div>
+      </div>
     </>
   );
 }
