46dea57f1ea27c0c85176d47e3ec90577a861981
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-11-21 10:58:42 +0100

    whitespaces

--- lib/langchain/validateIngredient.ts
+++ lib/langchain/validateIngredient.ts
@@ -9,37 +9,37 @@ const model = new ChatOpenAI({
 });
 
 const prompt = ChatPromptTemplate.fromTemplate(`
-    Evaluate if the given ingredient is an edible food product. Answer only "true" or "false".
-    
-    Ingredient to evaluate: {ingredient}
-    
-    Evaluation rules:
-    - Answer "true" if the ingredient is:
-      * A fruit or vegetable
-      * A food product available in stores
-      * A spice or herb
-      * Meat or fish
-      * Dairy
-      * Grain or its derivative
-      * An ingredient used in cooking
-      
-    - Answer "false" if the ingredient is:
-      * Inedible or toxic
-      * A random string of characters
-      * A non-food item
-      * Does not exist as a food product
-    
-    Examples:
-    "banana": true
-    "apple": true
-    "salt": true
-    "chicken": true
-    "XKCD123": false
-    "stone": false
-    "dirt": false
-    "poison": false
-    
-    Answer only "true" or "false".
+Evaluate if the given ingredient is an edible food product. Answer only "true" or "false".
+
+Ingredient to evaluate: {ingredient}
+
+Evaluation rules:
+- Answer "true" if the ingredient is:
+  * A fruit or vegetable
+  * A food product available in stores
+  * A spice or herb
+  * Meat or fish
+  * Dairy
+  * Grain or its derivative
+  * An ingredient used in cooking
+  
+- Answer "false" if the ingredient is:
+  * Inedible or toxic
+  * A random string of characters
+  * A non-food item
+  * Does not exist as a food product
+
+Examples:
+"banana": true
+"apple": true
+"salt": true
+"chicken": true
+"XKCD123": false
+"stone": false
+"dirt": false
+"poison": false
+
+Answer only "true" or "false".
 `);
 
 const chain = prompt.pipe(model);
