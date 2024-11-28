1bace0b703fc5d14398d1986c2578b2fe6919518
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-11-28 09:15:55 +0100

    UserPermissions in enum

--- app/api/admin/users/route.ts
+++ app/api/admin/users/route.ts
@@ -3,13 +3,14 @@ import {
   getServerSessionOrCauseUnathorizedError,
 } from "@/lib/apiUtils";
 import connectDB from "@/lib/connectToDatabase";
+import { UserPermissions } from "@/lib/permissions";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
 
 export const GET = withApiErrorHandling(async () => {
   await connectDB();
 
-  await getServerSessionOrCauseUnathorizedError(["read:users"]);
+  await getServerSessionOrCauseUnathorizedError([UserPermissions.readUsers]);
 
   const users = await User.find().select("-password");
 
