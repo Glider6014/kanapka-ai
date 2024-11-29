c05880c92c8d4161a5e514e9a9aeddf48b36ab23
Patryk-Ruzicki <arcysanta@gmail.com>
2024-11-29 12:31:31 +0100

    newsletter responsibility

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -57,10 +57,10 @@ export default function Newsletter() {
                     name="email"
                     render={({ field, fieldState }) => (
                       <FormItem className="flex flex-col items-center">
-                        <div className="flex items-center">
-                          <FormControl>
+                        <div className="flex flex-col md:flex-row items-center w-full">
+                          <FormControl className="w-full md:w-auto">
                             <Input
-                              className={`rounded-l-full text-opacity-70 focus-visible:text-opacity-100 border-solid border-2  focus-visible:ring-0 focus-visible:bg-opacity-20 border-black px-4 py-2 placeholder:text-white placeholder:text-opacity-70 placeholder:focus-visible:text-opacity-90 ${
+                              className={`rounded-t-md md:rounded-l-full mb-3 md:mb-0 text-opacity-70 focus-visible:text-opacity-100 border-solid border-2 focus-visible:ring-0 focus-visible:bg-opacity-20 border-black px-4 py-2 placeholder:text-white placeholder:text-opacity-70 placeholder:focus-visible:text-opacity-90 ${
                                 fieldState.invalid
                                   ? "text-red-500 border-red-500"
                                   : "text-white"
@@ -71,7 +71,7 @@ export default function Newsletter() {
                             />
                           </FormControl>
                           <Button
-                            className="rounded-r-full bg-black font-bold hover:bg-gradient-to-r from-purple-700 to-orange-500 text-white px-6 py-2 transition-transform hover:scale-105 duration-200"
+                            className="rounded-b-md md:rounded-r-full md:rounded-b-none bg-black font-bold hover:bg-gradient-to-r from-purple-700 to-orange-500 text-white px-6 py-2 transition-transform hover:scale-105 duration-200 w-full md:w-auto"
                             type="submit"
                           >
                             Sign up - it's free!
