75b8f980e99954c199907e9ee419469aeb8661e0
FylypekUNO <filip.dudzik.uno@gmail.com>
2024-11-30 10:54:39 +0100

    link fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,32 @@
-"use client";
+'use client';
 
-import FAQ from "@/components/FAQ";
-import Newsletter from "@/components/Newsletter";
-import Footer from "@/components/Footer";
-import { MainNavbar } from "@/components/MainNavbar";
-import Greeting from "@/components/Greeting";
-import People from "@/components/People";
-// import { people0, people1 } from "@/components/PeopleArr";
-import About_app from "@/components/About_app";
-import Features from "@/components/Features";
-import PromoBanner from "@/components/PromoBanner";
+import FAQ from '@/components/FAQ';
+import Newsletter from '@/components/Newsletter';
+import Footer from '@/components/Footer';
+import { MainNavbar } from '@/components/MainNavbar';
+import Greeting from '@/components/Greeting';
+import About_app from '@/components/About_app';
+import Features from '@/components/Features';
+import PromoBanner from '@/components/PromoBanner';
 
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto py-4">
+      <div className='container mx-auto py-4'>
         <MainNavbar />
       </div>
-      <div className="w-full">
+      <div className='w-full'>
         <Greeting />
         <About_app />
       </div>
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col items-center md:flex-row gap-4">
-          <div className="w-full flex-grow mt-4 md:mt-0">
+      <div className='container mx-auto p-4'>
+        <div className='flex flex-col items-center md:flex-row gap-4'>
+          <div className='w-full flex-grow mt-4 md:mt-0'>
             <Features />
           </div>
         </div>
       </div>
-      <div className="w-full">
+      <div className='w-full'>
         <PromoBanner />
         {/* 
         <People people={people0} isReverse={false} />
--- components/MainNavbar.tsx
+++ components/MainNavbar.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import Link from "next/link";
-import { Logo } from "@/components/Logo";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import * as React from 'react';
+import Link from 'next/link';
+import { Logo } from '@/components/Logo';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,36 +12,36 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { useState } from "react";
-import { Menu, X } from "lucide-react";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+} from '@/components/ui/navigation-menu';
+import { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 
 const components = [
   {
-    title: "Recipe Generator",
-    href: "/dashboard",
-    description: "Generate custom recipes based on your preferences.",
+    title: 'Recipe Generator',
+    href: '/dashboard',
+    description: 'Generate custom recipes based on your preferences.',
   },
   {
-    title: "Meal Planning",
-    href: "/meal-planning",
-    description: "Plan your meals for the week with ease.",
+    title: 'Meal Planning',
+    href: '/meal-planner',
+    description: 'Plan your meals for the week with ease.',
   },
   {
-    title: "Nutrition Info",
-    href: "/nutrition-info",
-    description: "Get detailed nutritional information for all your meals.",
+    title: 'Nutrition Info',
+    href: '/',
+    description: 'Get detailed nutritional information for all your meals.',
   },
   {
-    title: "Ingredient Database",
-    href: "/ingredient-database",
-    description: "Access a comprehensive database of ingredients.",
+    title: 'Ingredient Database',
+    href: '/',
+    description: 'Access a comprehensive database of ingredients.',
   },
   {
-    title: "Cooking Tips",
-    href: "/cooking-tips",
-    description: "Improve your cooking skills with expert tips.",
+    title: 'Cooking Tips',
+    href: '/',
+    description: 'Improve your cooking skills with expert tips.',
   },
 ];
 
@@ -49,25 +49,25 @@ export const MainNavbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <div className="relative flex items-center justify-between pl-4 pr-5 md:px-0 py-0 md:py-2 max-w-6xl mx-auto">
-      <div className="flex items-center space-x-4">
-        <Logo className="text-3xl md:text-4xl" />
-        <NavigationMenu className="hidden lg:flex items-center space-x-4">
-          <NavigationMenuList className="flex items-center space-x-4">
+    <div className='relative flex items-center justify-between pl-4 pr-5 md:px-0 py-0 md:py-2 max-w-6xl mx-auto'>
+      <div className='flex items-center space-x-4'>
+        <Logo className='text-3xl md:text-4xl' />
+        <NavigationMenu className='hidden lg:flex items-center space-x-4'>
+          <NavigationMenuList className='flex items-center space-x-4'>
             <NavigationMenuItem>
               <NavigationMenuTrigger>Features</NavigationMenuTrigger>
               <NavigationMenuContent>
-                <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
+                <ul className='grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                  <li className='row-span-3'>
                     <NavigationMenuLink asChild>
                       <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
+                        className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                        href='/'
                       >
-                        <div className="mb-2 mt-4 text-lg font-medium">
+                        <div className='mb-2 mt-4 text-lg font-medium'>
                           Kanapka AI
                         </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
+                        <p className='text-sm leading-tight text-muted-foreground'>
                           Your AI-powered recipe and meal planning assistant.
                         </p>
                       </a>
@@ -86,14 +86,14 @@ export const MainNavbar = () => {
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Link href="/community" legacyBehavior passHref>
+              <Link href='/' legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Community
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Link href="/pricing" legacyBehavior passHref>
+              <Link href='/pricing' legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                   Pricing
                 </NavigationMenuLink>
@@ -103,65 +103,65 @@ export const MainNavbar = () => {
         </NavigationMenu>
       </div>
 
-      <div className="hidden lg:flex items-center space-x-4">
+      <div className='hidden lg:flex items-center space-x-4'>
         <Button
-          variant="outline"
-          className="bg-black hover:bg-black hover:text-white text-white font-bold w-full md:w-auto"
-          onClick={() => (window.location.href = "/user/signin")}
+          variant='outline'
+          className='bg-black hover:bg-black hover:text-white text-white font-bold w-full md:w-auto'
+          onClick={() => (window.location.href = '/user/signin')}
         >
           LOGIN
         </Button>
         <Button
-          variant="outline"
-          className="text-white font-bold bg-gradient-to-r from-purple-700 to-orange-500 w-full md:w-auto hover:text-white"
-          onClick={() => (window.location.href = "/user/signup")}
+          variant='outline'
+          className='text-white font-bold bg-gradient-to-r from-purple-700 to-orange-500 w-full md:w-auto hover:text-white'
+          onClick={() => (window.location.href = '/user/signup')}
         >
           Get started for free
         </Button>
       </div>
 
       <button
-        className="block lg:hidden"
+        className='block lg:hidden'
         onClick={() => setIsMobileMenuOpen((prev) => !prev)}
       >
         {isMobileMenuOpen ? (
-          <X className="h-6 w-6" />
+          <X className='h-6 w-6' />
         ) : (
-          <Menu className="h-6 w-6" />
+          <Menu className='h-6 w-6' />
         )}
       </button>
 
       {isMobileMenuOpen && (
-        <div className="absolute top-16 left-0 right-0 bg-white shadow-md p-4 lg:hidden z-10">
-          <ul className="space-y-4">
+        <div className='absolute top-16 left-0 right-0 bg-white shadow-md p-4 lg:hidden z-10'>
+          <ul className='space-y-4'>
             <li>
               <Button
-                variant="outline"
-                className="bg-black hover:bg-black hover:text-white text-white font-bold hover:rounded-none w-full md:w-auto"
-                onClick={() => (window.location.href = "/user/signin")}
+                variant='outline'
+                className='bg-black hover:bg-black hover:text-white text-white font-bold hover:rounded-none w-full md:w-auto'
+                onClick={() => (window.location.href = '/user/signin')}
               >
                 LOGIN
               </Button>
             </li>
             <li>
               <Button
-                variant="outline"
-                className="text-white hover:rounded-none font-bold bg-gradient-to-r from-purple-700 to-orange-500 w-full md:w-auto hover:text-white"
-                onClick={() => (window.location.href = "/user/signup")}
+                variant='outline'
+                className='text-white hover:rounded-none font-bold bg-gradient-to-r from-purple-700 to-orange-500 w-full md:w-auto hover:text-white'
+                onClick={() => (window.location.href = '/user/signup')}
               >
                 Get started for free
               </Button>
             </li>
             <li>
-              <Link href="/community" passHref>
-                <span className="block text-gray-800 hover:text-black">
+              <Link href='/community' passHref>
+                <span className='block text-gray-800 hover:text-black'>
                   Community
                 </span>
               </Link>
             </li>
             <li>
-              <Link href="/pricing" passHref>
-                <span className="block text-gray-800 hover:text-black">
+              <Link href='/pricing' passHref>
+                <span className='block text-gray-800 hover:text-black'>
                   Pricing
                 </span>
               </Link>
@@ -169,7 +169,7 @@ export const MainNavbar = () => {
             {components.map((component) => (
               <li key={component.title}>
                 <Link href={component.href} passHref>
-                  <span className="block text-gray-800 hover:text-black">
+                  <span className='block text-gray-800 hover:text-black'>
                     {component.title}
                   </span>
                 </Link>
@@ -183,8 +183,8 @@ export const MainNavbar = () => {
 };
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
 >(({ className, title, children, ...props }, ref) => {
   return (
     <li>
@@ -192,13 +192,13 @@ const ListItem = React.forwardRef<
         <a
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
           {...props}
         >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          <div className='text-sm font-medium leading-none'>{title}</div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
             {children}
           </p>
         </a>
@@ -206,4 +206,4 @@ const ListItem = React.forwardRef<
     </li>
   );
 });
-ListItem.displayName = "ListItem";
+ListItem.displayName = 'ListItem';
